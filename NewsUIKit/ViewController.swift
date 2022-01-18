f7dc71f87e39ff6fdb7966971e35b065b25c3f25
Siarhei87 <75639575+Siarhei87@users.noreply.github.com>
2022-01-18 17:39:20 +0300

    Initial Commit

---
+++ NewsUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsUIKit
+//
+//  Created by Сергей Дубко on 18.01.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
